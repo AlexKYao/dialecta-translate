59e204c2a9b8caee9f06dfe4268cdba2c88978be
Alexander Yao <9095664+AlexKYao@users.noreply.github.com>
2023-02-06 13:05:59 -0600

    Translate Feature Uploaded

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,51 +6,72 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = 
-`Give me a list 5 Spanish most-used vocabulary words for the theme below.
-
-Theme:
-`;
 const generateAction = async (req, res) => {
-  // Run first prompt
-  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+  // Run the prompt
+  console.log(`API: Act as a ${req.body.selectedLanguage} translator, spelling corrector and improver.`)
 
-  const baseCompletion = await openai.createCompletion({
+  const completion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    prompt: `I want you to act as a ${req.body.selectedLanguage} translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved conversational version of my text, in ${req.body.selectedLanguage}. Generate the response in a Github style markdown table with each original sentence and translated sentence in the same row with the first language name as the heading in the first column and the second language name as the heading in the second column.
+
+    Translate this text: ${req.body.userInput}`,
     temperature: 0.7,
     max_tokens: 250,
   });
   
-  const basePromptOutput = baseCompletion.data.choices.pop();
-
-  // I build Prompt #2.
-  const secondPrompt = 
-  `
-  Take the list of vocabulary and theme below and generate five Spanish sentences in an informal style. Output both Spanish and English in two columns.
-
-  Theme: ${req.body.userInput}
-
-  Vocabulary: ${basePromptOutput.text}
-
-  Sentences:
-  `
-
-  // I call the OpenAI API a second time with Prompt #2
-  const secondPromptCompletion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: `${secondPrompt}`,
-    // I set a higher temperature for this one. Up to you!
-    temperature: 0.7,
-		// I also increase max_tokens.
-    max_tokens: 1250,
-  });
-  
   // Get the output
-  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+  const output = completion.data.choices.pop();
 
-  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
-  res.status(200).json({ output: secondPromptOutput });
+  // Send over the output to the UI
+  res.status(200).json({ output });
 };
+
+
+// const basePromptPrefix = 
+// `Give me a list 5 Spanish most-used vocabulary words for the theme below.
+
+// Theme:
+// `;
+// const generateAction = async (req, res) => {
+//   // Run first prompt
+//   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+//   const baseCompletion = await openai.createCompletion({
+//     model: 'text-davinci-003',
+//     prompt: `${basePromptPrefix}${req.body.userInput}`,
+//     temperature: 0.7,
+//     max_tokens: 250,
+//   });
+  
+//   const basePromptOutput = baseCompletion.data.choices.pop();
+
+//   // I build Prompt #2.
+//   const secondPrompt = 
+//   `
+//   Take the list of vocabulary and theme below and generate five Spanish sentences in an informal style. Output both Spanish and English in two columns.
+
+//   Theme: ${req.body.userInput}
+
+//   Vocabulary: ${basePromptOutput.text}
+
+//   Sentences:
+//   `
+
+//   // I call the OpenAI API a second time with Prompt #2
+//   const secondPromptCompletion = await openai.createCompletion({
+//     model: 'text-davinci-003',
+//     prompt: `${secondPrompt}`,
+//     // I set a higher temperature for this one. Up to you!
+//     temperature: 0.7,
+// 		// I also increase max_tokens.
+//     max_tokens: 1250,
+//   });
+  
+//   // Get the output
+//   const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+//   // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+//   res.status(200).json({ output: secondPromptOutput });
+// };
 
 export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
+// @ts-nocheck
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 import { useState } from 'react';
-
+import React from 'react';
+import generateTable from './generatetable';
 
 const Home = () => {
+  // Single Language Selection
+  const [selectedLanguage, setSelectedLanguage] = useState('');
+  
+  // User input from the textbox
   const [userInput, setUserInput] = useState('');
 
   const [apiOutput, setApiOutput] = useState('')
   const [isGenerating, setIsGenerating] = useState(false)
-  
+
   const callGenerateEndpoint = async () => {
     setIsGenerating(true);
     
@@ -19,7 +25,7 @@ const Home = () => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ userInput }),
+      body: JSON.stringify({ userInput , selectedLanguage }), 
     });
   
     const data = await response.json();
@@ -27,25 +33,49 @@ const Home = () => {
     console.log("OpenAI replied...", output.text)
   
     setApiOutput(`${output.text}`);
+    
     setIsGenerating(false);
+  }
+  
+  
+
+  const handleLanguageChange = (event) => {
+    setSelectedLanguage(event.target.value)
   }
 
   const onUserChangedText = (event) => {
     setUserInput(event.target.value);
   };
 
+  
+
   return (
     <div className="root">
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Learn sentences in your target language</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>Input the topic</h2>
+            <h1>Translate anything into your target language</h1>
           </div>
         </div>
+        
+
         <div className="prompt-container">
+
+        <div className="header-subtitle">
+            <h2>1. Choose your Target Language</h2>
+          </div>
+        <div className='dropdown-container'>
+          <select value={selectedLanguage} onChange={handleLanguageChange}>
+            <option value="English">English</option>
+            <option value="Spanish">Spanish</option>
+            <option value="French">French</option>
+            <option value="German">German</option>
+          </select>
+          {/* <p>Selected Language: {selectedLanguage}</p> */}
+        </div>
+           <div className="header-subtitle">
+            <h2>2. Input the sentence or paragraph(s)</h2>
+          </div>
           <textarea placeholder="start typing here" className="prompt-box" value={userInput}
           onChange={onUserChangedText}/>
           <div className="prompt-buttons">
@@ -65,6 +95,11 @@ const Home = () => {
               </div>
             </div>
             <div className="output-content">
+              
+              <div className='output-table'>
+                {generateTable(apiOutput)}
+              </div>
+
               <p>{apiOutput}</p>
             </div>
           </div>
