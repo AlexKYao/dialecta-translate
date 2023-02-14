7795b7eab1d23c63bb5dc74dbbf9fe6bd17c63df
Alexander Yao <9095664+AlexKYao@users.noreply.github.com>
2023-02-13 21:32:35 -0600

    removed feedback form

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,9 @@ import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 import { useState } from 'react';
 import React from 'react';
-import { FeedbackFish } from '@feedback-fish/react'
 
 
 const Home = () => {
-  const projectId = '4f2e3d1f702689';
   // Single Language Selection
   const [selectedLanguage, setSelectedLanguage] = useState('');
   
@@ -132,11 +130,6 @@ const Home = () => {
           </div>
         )}
         </div>
-        <div className="button">
-            <FeedbackForm triggerComponent={Button}
-            projectId={projectId}
-          />
-          </div>
       </div>
     </div>
   );
